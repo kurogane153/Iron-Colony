80ce80c409a269a80d518872547a1ba87ef1c99a
kurogane153 <11807067@stu.kbc.ac.jp>
2019-10-25 09:42:10 +0900

    スクロール

---
+++ Assets/mayu/Script/Background.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Background : MonoBehaviour {
+
+    // スクロールするスピード
+    public float speed = 0.1f;
+    private GameObject playey;
+
+    void Start()
+    {
+        playey = GameObject.Find("mairo");
+    }
+
+    void Update()
+    {
+        // 時間によってYの値が0から1に変化していく。1になったら0に戻り、繰り返す。
+        float x = Mathf.Repeat(playey.transform.position.x * speed, 1);
+
+        // Yの値がずれていくオフセットを作成
+        Vector2 offset = new Vector2(x, 0);
+
+        // マテリアルにオフセットを設定する
+        GetComponent<Renderer>().sharedMaterial.SetTextureOffset("_MainTex", offset);
+    }
+}
---
+++ Assets/mayu/Script/Player.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private float dir;
+    public float speed=0.01f;
+
+    void Start()
+    {
+        dir = Random.Range(0, 360);
+    }
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Translate(speed, 0, 0);
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Translate(-speed, 0, 0);
+        }
+        if (Input.GetKey(KeyCode.Space))
+        {
+            transform.Rotate(0, 0, dir);
+        }
+    }
+}

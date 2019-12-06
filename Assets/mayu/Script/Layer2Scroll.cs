d8cc3a2540e73d61d0ee5f20d54452766416a281
kurogane153 <11807067@stu.kbc.ac.jp>
2019-12-06 12:59:02 +0900

    ステージ修正ver1.0

---
+++ Assets/mayu/Script/Layer2Scroll.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Layer2Scroll : MonoBehaviour {
+
+    float back_x = 0;
+    private GameObject player;
+    private Rigidbody2D rb;
+    private float speed = 0.02f;
+    public int spriteCount = 3;
+
+    void Start()
+    {
+        player = GameObject.Find("Mairo");
+        rb = player.GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        if (back_x != player.transform.position.x )
+        {
+            if (player.transform.position.x > back_x && rb.velocity.x >= 0.9f)
+            {
+                transform.position += Vector3.right * speed * 2.0f;
+            }
+            else if (player.transform.position.x < back_x && rb.velocity.x <= -0.9f)
+            {
+                transform.position += Vector3.left * speed * 2.0f;
+            }
+        }
+        else if (back_x == player.transform.position.x)
+        {
+            transform.position += Vector3.left * 0.0f;
+            transform.position += Vector3.right * 0.0f;
+        }
+        back_x = player.transform.position.x;
+    }
+
+    void OnBecameInvisible()
+    {
+        // スプライトの幅を取得
+        float width = GetComponent<SpriteRenderer>().bounds.size.x;
+        // 幅ｘ個数分だけ右へ移動
+        transform.position += Vector3.left * width * spriteCount;
+    }
+}
--- Assets/mayu/Script/TestScroll.cs
+++ Assets/mayu/Script/TestScroll.cs
@@ -6,35 +6,44 @@ public class TestScroll : MonoBehaviour
 {
     float back_x = 0;
     private GameObject player;
-    public float speed = 10;
+    private Rigidbody2D rb;
+    private float speed = 0.01f;
     public int spriteCount = 3;
 
     void Start()
     {
         player = GameObject.Find("Mairo");
+        rb = player.GetComponent<Rigidbody2D>();
     }
 
     void Update()
     {
-        if (back_x != player.transform.position.x)
+        if (back_x != player.transform.position.x )
         {
-            if (player.transform.position.x > 0)
+            if (player.transform.position.x > 0 )
             {
-                // 左へ移動
-                transform.position += Vector3.right * speed * player.transform.position.x;
+                if (player.transform.position.x > back_x && rb.velocity.x >= 0.9f)
+                {
+                    transform.position += Vector3.right * speed * 2.0f ;
+
+                }else if (player.transform.position.x < back_x && rb.velocity.x <= -0.9f)
+                {
+                    transform.position += Vector3.left * speed * 2.0f ;
+                }
             }
         }else if (back_x == player.transform.position.x)
         {
-            transform.position +=Vector3.right * 0.0f;
+            transform.position += Vector3.left * speed * 0.0f;
+            transform.position += Vector3.right * speed * 0.0f;
         }
         back_x = player.transform.position.x;
     }
 
-    void OnBecameInvisible()
-    {
-        // スプライトの幅を取得
-        float width = GetComponent<SpriteRenderer>().bounds.size.x;
-        // 幅ｘ個数分だけ右へ移動
-        transform.position += Vector3.left * width * spriteCount;
-    }
+    //void OnBecameInvisible()
+    //{
+    //    // スプライトの幅を取得
+    //    float width = GetComponent<SpriteRenderer>().bounds.size.x;
+    //    // 幅ｘ個数分だけ右へ移動
+    //    transform.position += Vector3.left * width * spriteCount;
+    //}
 }

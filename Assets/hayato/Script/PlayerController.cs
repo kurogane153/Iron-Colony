3014d9c82ddda69969198aa78a61490c96cd8e9e
rongbiiti <11807078@stu.kbc.ac.jp>
2019-12-06 12:43:03 +0900

    音をファイルに入れた

--- Assets/hayato/Script/PlayerController.cs
+++ Assets/hayato/Script/PlayerController.cs
@@ -86,7 +86,7 @@ public class PlayerController : MonoBehaviour
             rb.AddForce(new Vector2(playerManager.MoveForceMultiplier * (inputManager.MoveKey * playerManager.MoveSpeed - rb.velocity.x), rb.velocity.y));
 
             if (isJumpingCheck && inputManager.JumpKey != 0) {
-                SoundManager.Instance.PlaySeByName("Jump");
+                SoundManager.Instance.PlaySeByName("Jump_2");
                 jumpTimeCounter = playerManager.JumpTime;
                 isJumpingCheck = false;
                 isJumping = true;

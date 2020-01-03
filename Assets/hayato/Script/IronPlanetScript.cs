ee704ec895483c84579274c6bae377a1ace3bdee
rongbiiti <11807078@stu.kbc.ac.jp>
2020-01-03 21:51:35 +0900

    ラスボス戦の発狂モードと通常モードの分岐、テスラキャノンのチャージ系の処理などを書いた

--- Assets/hayato/Script/IronPlanetScript.cs
+++ Assets/hayato/Script/IronPlanetScript.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class IronPlanetScript : MonoBehaviour {
 
@@ -11,6 +12,7 @@ public class IronPlanetScript : MonoBehaviour {
     public int angleNumber;
 
     private float teslaChargeTime;  // テスラキャノンチャージ時間
+    private float teslaCapacity;    // テスラキャノンのチャージ容量（可変）
     private int powerDustGetCount;  // パワーダストを取った回数
     private float playerHP;         // プレイヤーHP 
     [SerializeField] private float HPDamage = 20f;  //  HPダメージ量
@@ -26,10 +28,11 @@ public class IronPlanetScript : MonoBehaviour {
         playerManager = PlayerManager.Instance;
         rb = GetComponent<Rigidbody2D>();
         playerHP = _playerStartHP;
+        teslaCapacity = 33f;
     }
 	
 	void Update () {
-        if (!isRotating) {      // 回転していないときは、キー入力を受け取る。
+        if (!isRotating && inputManager.JumpKey == 0) {      // 回転していないときは、キー入力を受け取る。
             if (inputManager.RotateLeftKey) {
                 RotatingNow(180);
 
@@ -42,6 +45,16 @@ public class IronPlanetScript : MonoBehaviour {
             if (rotateTimer <= 0) {
                 isRotating = false;
             }
+        }
+
+        if(inputManager.JumpKey == 2) {
+            if(teslaChargeTime <= teslaCapacity) {
+                teslaChargeTime += 100 / teslaMaxCharge * Time.deltaTime;
+                if(teslaCapacity < teslaChargeTime) {
+                    teslaChargeTime = teslaCapacity;
+                }
+            }
+            Debug.Log("テスラキャノン量      " + teslaChargeTime);
         }
     }
 
@@ -78,22 +91,40 @@ public class IronPlanetScript : MonoBehaviour {
     private void Damage()
     {
         playerHP -= HPDamage;
+        Debug.Log("現在のHP " + playerHP + " / " + _playerStartHP);
+        if(playerHP <= 0) {
+            // 現在のScene名を取得する
+            Scene loadScene = SceneManager.GetActiveScene();
+            // Sceneの読み直し
+            SceneManager.LoadScene(loadScene.name);
+        }
     }
 
     // パワーダストを取ったとき、テスラキャノンのチャージ上限を解放する。最大で2まで増える。
     private void PowerCharge()
     {
-        if(powerDustGetCount < 3) {
+        if(powerDustGetCount < 2) {
             powerDustGetCount++;
+            switch (powerDustGetCount) {
+                case 1:
+                    teslaCapacity = 66f;
+                    break;
+                case 2:
+                    teslaCapacity = 100f;
+                    break;
+            }
         }
+        Debug.Log("現在のチャージ上限 " + teslaCapacity);
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if(collision.gameObject.tag == "StarDust") {
             Damage();
+            Debug.Log("ダストが当たってプレイヤーにダメージ！");
         } else if (collision.gameObject.tag == "PowerDust") {
             Damage();
+            Debug.Log("ダストが当たってプレイヤーにダメージ！");
         }
     }
 
@@ -101,8 +132,10 @@ public class IronPlanetScript : MonoBehaviour {
     {
         if(collision.tag == "StarDust") {
             Damage();
+            Debug.Log("ダストが当たってプレイヤーにダメージ！");
         } else if (collision.tag == "PowerDust") {
             PowerCharge();
+            Debug.Log("パワーダストを吸収してチャージ容量が増えた！");
         }
     }
 }
--- Assets/hayato/Script/LastBossScript.cs
+++ Assets/hayato/Script/LastBossScript.cs
@@ -12,6 +12,8 @@ public class LastBossScript : MonoBehaviour {
     [SerializeField] private GameObject StarDust;
     [SerializeField] private GameObject PowerDust;
 
+    [SerializeField] private float _bossStartHP = 300f;
+
     [SerializeField] private float _dustThrowRate_Normal = 8f;
     [SerializeField] private float _dustThrowRate_Crazy = 5f;
 
@@ -19,14 +21,59 @@ public class LastBossScript : MonoBehaviour {
     [SerializeField] private int _powerDustThrowTime_Crazy = 5;
 
     void Start () {
-		
+        bossHp = _bossStartHP;
 	}
 	
 	void Update () {
         dustThrowTimer += Time.deltaTime;
-		if(_dustThrowRate_Normal <= dustThrowTimer) {
-            Instantiate(StarDust);
-            dustThrowTimer = 0;
+        if (isCrazyMode) {
+            if (_dustThrowRate_Crazy <= dustThrowTimer) {
+                ++dustThrowCount;
+                Debug.Log("ダスト投てき " + dustThrowCount + "回目");
+                Instantiate(InstantDust());
+                dustThrowTimer = 0;
+            }
+        } else {
+            if (_dustThrowRate_Normal <= dustThrowTimer) {
+                ++dustThrowCount;
+                Debug.Log("ダスト投てき " + dustThrowCount + "回目");
+                Instantiate(InstantDust());
+                dustThrowTimer = 0;
+            }
+            HPCheck();
         }
+        
 	}
+
+    private GameObject InstantDust()
+    {
+        if (isCrazyMode) {
+            if(dustThrowCount == _powerDustThrowTime_Crazy) {
+                Debug.Log("発狂モードのラスボスがパワーダストを投げた！");
+                dustThrowCount = 0;
+                return PowerDust;
+            } else {
+                Debug.Log("発狂モードのラスボスがふつうのダストを投げた！");
+                return StarDust;
+            }
+        } else {
+            if(dustThrowCount == _powerDustThrowTime_Normal) {
+                Debug.Log("通常モードのラスボスがパワーダストを投げた！");
+                bossHp -= 200f;
+                dustThrowCount = 0;
+                return PowerDust;
+            } else {
+                Debug.Log("通常モードのラスボスがふつうのダストを投げた！");
+                return StarDust;
+            }
+        }
+    }
+
+    private void HPCheck()
+    {
+        if(bossHp <= _bossStartHP / 3) {
+            isCrazyMode = true;
+            Debug.Log("ラスボスは発狂モードになった！！");
+        }
+    }
 }

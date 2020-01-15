108da14077a64afddb7c117db3fc0dd2ba018811
rongbiiti <11807078@stu.kbc.ac.jp>
2020-01-15 11:08:04 +0900

    お花が生えてくるスクリプトを作った

---
+++ Assets/hayato/Script/GrowFlower.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrowFlower : MonoBehaviour {
+
+    private bool isTouchMaxGrowPoint;   // 成長できる最高点に触れたか
+
+	void Start () {
+		
+	}
+	
+	void Update () {
+		if(!isTouchMaxGrowPoint) {
+            transform.Translate(0, 0.01f, 0);
+        }
+	}
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(!isTouchMaxGrowPoint && collision.tag == "FlowerMaxGrowPoint") {
+            isTouchMaxGrowPoint = true;
+            Debug.Log("ぐれた");
+        }
+    }
+}
---
+++ Assets/hayato/Script/MoveFlowerSeed.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveFlowerSeed : MonoBehaviour {
+
+    private bool isSetFlowerStand;
+    [SerializeField] private GameObject _flowerInstance;
+    private BoxCollider2D boxCollider;
+    private Rigidbody2D rigidbody2;
+    private MovableMagnetContoroller magnetContoroller;
+
+	void Start () {
+        boxCollider = GetComponent<BoxCollider2D>();
+        rigidbody2 = GetComponent<Rigidbody2D>();
+        magnetContoroller = GetComponent<MovableMagnetContoroller>();
+	}
+	
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "FlowerStand" && !isSetFlowerStand) {
+            Instantiate(_flowerInstance, transform.position - new Vector3(0, 1.3f, 0), Quaternion.identity);
+            isSetFlowerStand = true;
+            gameObject.tag = "Ground";
+            gameObject.layer = LayerMask.NameToLayer("Default");
+            rigidbody2.bodyType = RigidbodyType2D.Static;
+            magnetContoroller.enabled = false;
+        }
+    }
+}
